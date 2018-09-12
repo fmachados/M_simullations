27358a559d2e9c1fef8832307fe6f418d36780f3
Marlon E. Cobos <manubio13@gmail.com>
2018-09-12 00:16:06 -0500

    Details added in the part that R will be taking care of

--- R_python_V/kuenm_m.R
+++ R_python_V/kuenm_m.R
@@ -19,12 +19,18 @@
 #' (according to the \code{ext.type}) containing results of the hierarchical partitioning of the variance 
 #' of models will be written. Default = "Hierarchical_partitioning".
 #' @param mask.vars (logical) whether or not to mask the variables to the simulated accessible area (M).
+#' Default = TRUE.
 #' @param out.varmasked (character) name of the folder to be created to save the masked variables.
+#' Default = "M_variables".
+#' @param plot (logical) whether or not to plot the resultant M on a environmental predictor, 
+#' including the species occurrences as well. Default = TRUE.
 #' 
 #' @return Folder \code{out.dir} conatining the results of the simulation. These results include: 
-#'. A plot of the M and the 
-#' occurrences on an environmental layer map will return to the plot window. If \code{mask.vars} = TRUE,
-#' the environmental variables will be masked to the M and written in the \code{out.varmasked} folder.
+#' A plot of the M and the occurrences on an environmental layer map that will return to a 
+#' graphic device if \code{plot} = TRUE; the M as a shapefile and as a raster layer in ascii format;
+#' a folder with ....  
+#' If \code{mask.vars} = TRUE, the environmental variables will be masked to the M and the masked 
+#' layers will be written in the \code{out.varmasked} folder.
 #'
 #' @details 
 
@@ -32,15 +38,15 @@
 
 kuenm_m <- function(occ, vars.folder, disp.function = "normal", function.spread = 2, 
                     n.dispersors = 4, suit.threshold = 5, n.rep = 100, 
-                    access.threshold = 5, out.dir = "M", mask.vars = FALSE, 
-                    out.varmasked = "M_variables") {
+                    access.threshold = 5, out.dir = "M", mask.vars = TRUE, 
+                    out.varmasked = "M_variables", plot = TRUE) {
   
   # install the package if needed
   if(!require(reticulate)){
     install.packages("reticulate")
   }
   
-  # testing for initial requirements and setting R up
+  # testing for initial requirements and setting R up TRY SOMETHING TO INSTALL AND DEFINE WHERE PYTHON IS... SYSTEM?
   
   reticulate::py_available() # cuando veas algo asi name::name es para usar una funcion de un paquete sin llamarlo
   
@@ -54,22 +60,34 @@ kuenm_m <- function(occ, vars.folder, disp.function = "normal", function.spread 
   
   ## records
   occ <- read.csv(occ)
-
+  
+  ## data in records
+  occ_data <- data.frame(occ, raster::extract(variables, occ[, 2:3]))
+  colnames(occ_data) <- c(colnames(occ), var_names)
+  
   # python simulation
   reticulate::repl_python()
   
-  #ALL PYHON CODE
+  #ALL PYHON CODE HERE
   
   exit
   
   # preparing, writing, and outputs
-  ## 
+  ## records with environmental data
+  write.csv(occ_data, "occurrences_env_data.csv", row.names = FALSE)
   
-  ## writing M variables if asked
+  ## M
+  ### raster
+  
+  
+  ### shapefile
+  
+  
+  ## variables masked to M, if asked
   if (mask.vars == TRUE) {
     cat("Masking variables to M and writing them in", out.varmasked, "    Please wait...")
-    m_variables <- raster::mask(variables, m)
     m_variables <- raster::crop(m_variables, m)
+    m_variables <- raster::mask(variables, m)
     
     var_names <- paste(paste(out.varmasked, var_names, sep = "/"), ".asc", sep = "")
     
@@ -81,5 +99,15 @@ kuenm_m <- function(occ, vars.folder, disp.function = "normal", function.spread 
   }
   
   ## plot
+  if (.Platform$OS.type == "unix") {
+    quartz()
+  } else {
+    x11()
+  }
+  par(mar = c(0.5, 0.5, 0.5, 0.5))
+  image(variables[[1]])
+  plot(m, add = TRUE, lwd = 2, col = "blue3")
+  points(occ[, 2:3], pch = 19)
+  box()
   
 }
